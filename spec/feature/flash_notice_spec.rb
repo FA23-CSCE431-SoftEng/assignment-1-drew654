176910a7fae9a24c281d3ab3dfdd3c59c01b44c5
Drew Pusey <drewpusey654@gmail.com>
2023-09-04 21:09:18 -0500

    Added new book attributes to flash notice tests

--- spec/feature/flash_notice_spec.rb
+++ spec/feature/flash_notice_spec.rb
@@ -4,6 +4,9 @@ RSpec.feature 'Flash Notice', type: :feature do
     scenario 'user sees a flash notice after creating a book' do
         visit new_book_path
         fill_in "book[title]", with: 'Harry Potter'
+        fill_in "book[author]", with: 'J.K. Rowling'
+        fill_in "book[price]", with: 6.98
+        fill_in "book[published_date]", with: '1997-06-26'
         click_on 'Create Book'
         expect(page).to have_content('Book Harry Potter was successfully created.')
     end
@@ -16,7 +19,12 @@ RSpec.feature 'Flash Notice', type: :feature do
     end
 
     scenario 'user sees a flash notice after updating a book' do
-        book = Book.create(title: 'Harry Potter')
+        book = Book.create(
+            title: 'Harry Potter',
+            author: 'J.K. Rowling',
+            price: 6.98,
+            published_date: '1997-06-26'
+        )
         visit edit_book_path(book)
         fill_in "book[title]", with: 'Harry Potter and the Chamber of Secrets'
         click_on 'Update Book'
@@ -24,7 +32,12 @@ RSpec.feature 'Flash Notice', type: :feature do
     end
 
     scenario 'user sees a flash notice after deleting a book' do
-        book = Book.create(title: 'Harry Potter')
+        book = Book.create(
+            title: 'Harry Potter',
+            author: 'J.K. Rowling',
+            price: 6.98,
+            published_date: '1997-06-26'
+        )
         visit delete_book_path(book)
         click_on 'Delete'
         expect(page).to have_content('Book Harry Potter was successfully destroyed.')
