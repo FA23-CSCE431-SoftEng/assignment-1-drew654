578d1b581829c549058c945478157102e78f0279
Drew Pusey <drewpusey654@gmail.com>
2023-09-04 21:14:08 -0500

    Updated tests from starter code

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,12 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
     subject do
-        described_class.new(title: 'Harry Potter')
+        described_class.new(
+            title: 'Harry Potter',
+            author: 'J.K. Rowling',
+            price: 6.98,
+            published_date: '1997-06-26'
+        )
     end
 
     it 'is valid with valid attributes' do
